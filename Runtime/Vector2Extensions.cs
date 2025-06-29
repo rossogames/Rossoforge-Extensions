91e6d7666123d8a04d191cdbe9e87ab743b358d8
Agustin Rosso <AgustinRosso_1@hotmail.com>
2025-06-29 23:16:52 +0200

    se completan metodos de extension faltantes

--- Runtime/Vector2Extensions.cs
+++ Runtime/Vector2Extensions.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace RossoGames.SDK.Extensions.Unity
+namespace RossoForge.Extensions
 {
     public static class Vector2Extensions
     {
@@ -29,11 +29,10 @@ namespace RossoGames.SDK.Extensions.Unity
             float sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
             float cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
 
-            float tx = v.x;
-            float ty = v.y;
-            v.x = (cos * tx) - (sin * ty);
-            v.y = (sin * tx) + (cos * ty);
-            return v;
+            return new Vector2(
+              (cos * v.x) - (sin * v.y),
+              (sin * v.x) + (cos * v.y)
+          );
         }
 
         public static Vector2 RotateAround(this Vector2 v, Vector2 pivot, float degrees)
@@ -46,11 +45,10 @@ namespace RossoGames.SDK.Extensions.Unity
         /// <param name="v">current vector</param>
         /// <param name="scale">scale to apply</param>
         /// <returns></returns>
-        public static Vector2 Scale(this Vector2 v, float scale)
+        public static void Scale(this Vector2 v, float scale)
         {
             v.x *= scale;
             v.y *= scale;
-            return v;
         }
 
         /// <summary>
@@ -59,11 +57,10 @@ namespace RossoGames.SDK.Extensions.Unity
         /// <param name="v">current vector</param>
         /// <param name="scale">scale to apply</param>
         /// <returns></returns>
-        public static Vector2 Scale(this Vector2 v, Vector2 scale)
+        public static void Scale(this Vector2 v, Vector2 scale)
         {
             v.x *= scale.x;
             v.y *= scale.y;
-            return v;
         }
 
         /// <summary>
@@ -102,15 +99,80 @@ namespace RossoGames.SDK.Extensions.Unity
         /// Dot Product of two vectors.
         /// </summary>
         /// <param name="v">current vector</param>
+        /// <param name="target">The vector to compute the dot product with.</param>
         /// <returns></returns>
         public static float Dot(this Vector2 v, Vector2 target)
         {
             return Vector2.Dot(v, target);
         }
 
+        /// <summary>
+        /// Computes the 2D cross product (also known as the perp-dot product) between this vector and the target vector.
+        /// This returns a scalar value equal to (v.x * target.y) - (v.y * target.x).
+        /// The sign indicates the orientation: positive if the target vector is to the left (counter-clockwise),
+        /// negative if to the right (clockwise), and zero if the vectors are colinear.
+        /// </summary>
+        /// <param name="v">The first vector.</param>
+        /// <param name="target">The second vector to compute the cross product with.</param>
+        /// <returns>A signed scalar representing the magnitude and orientation of the cross product.</returns>
         public static float Cross(this Vector2 v, Vector2 target)
         {
             return (v.x * target.y) - (v.y * target.x);
+        }
+
+        /// <summary>
+        /// Returns a copy of this vector with the X component replaced by the specified value.
+        /// </summary>
+        /// <param name="v">The current vector.</param>
+        /// <param name="x">The new X value.</param>
+        /// <returns>A new vector with the updated X component.</returns>
+        public static Vector2 WithX(this Vector2 v, float x)
+        {
+            return new Vector2(x, v.y);
+        }
+
+        /// <summary>
+        /// Returns a copy of this vector with the Y component replaced by the specified value.
+        /// </summary>
+        /// <param name="v">The current vector.</param>
+        /// <param name="y">The new Y value.</param>
+        /// <returns>A new vector with the updated Y component.</returns>
+        public static Vector2 WithY(this Vector2 v, float y)
+        {
+            return new Vector2(v.x, y);
+        }
+
+        /// <summary>
+        /// Returns the projection of this vector onto the target vector.
+        /// </summary>
+        /// <param name="v">The current vector.</param>
+        /// <param name="target">The vector to project onto.</param>
+        /// <returns>A new vector representing the projection.</returns>
+        public static Vector2 ProjectOn(this Vector2 v, Vector2 target)
+        {
+            return Vector2.Dot(v, target) / target.sqrMagnitude * target;
+        }
+
+        /// <summary>
+        /// Returns the reflection of this vector off the plane defined by the given normal.
+        /// </summary>
+        /// <param name="v">The current vector.</param>
+        /// <param name="normal">The normal vector of the plane to reflect off. Must be normalized.</param>
+        /// <returns>A new vector representing the reflection.</returns>
+        public static Vector2 Reflect(this Vector2 v, Vector2 normal)
+        {
+            return v - 2 * Vector2.Dot(v, normal) * normal;
+        }
+
+        /// <summary>
+        /// Converts this Vector2 to a Vector3 with the specified Z component.
+        /// </summary>
+        /// <param name="v">The current vector.</param>
+        /// <param name="z">The Z component to use.</param>
+        /// <returns>A Vector3 with X, Y from this vector and the specified Z.</returns>
+        public static Vector3 ToVector3(this Vector2 v, float z = 0f)
+        {
+            return new Vector3(v.x, v.y, z);
         }
     }
 }
---
+++ Runtime/Vector3Extensions.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+
+namespace RossoForge.Extensions
+{
+    public static class Vector3Extensions
+    {
+        /// <summary>
+        /// Clamps each component of this vector between the given minimum and maximum values.
+        /// </summary>
+        public static Vector3 Clamp(this Vector3 vector, float min, float max)
+        {
+            return new Vector3(
+                Mathf.Clamp(vector.x, min, max),
+                Mathf.Clamp(vector.y, min, max),
+                Mathf.Clamp(vector.z, min, max)
+            );
+        }
+
+        /// <summary>
+        /// Rotates this vector around the given axis by the specified degrees.
+        /// </summary>
+        public static Vector3 RotateAroundAxis(this Vector3 v, Vector3 axis, float degrees)
+        {
+            return Quaternion.AngleAxis(degrees, axis) * v;
+        }
+
+        /// <summary>
+        /// Rotates this vector around a pivot point and axis by the specified degrees.
+        /// </summary>
+        public static Vector3 RotateAround(this Vector3 v, Vector3 pivot, Vector3 axis, float degrees)
+        {
+            return Quaternion.AngleAxis(degrees, axis) * (v - pivot) + pivot;
+        }
+
+        /// <summary>
+        /// Scales this vector uniformly by the specified scale factor.
+        /// </summary>
+        public static Vector3 Scale(this Vector3 v, float scale)
+        {
+            return v * scale;
+        }
+
+        /// <summary>
+        /// Scales this vector component-wise by another vector.
+        /// </summary>
+        public static Vector3 Scale(this Vector3 v, Vector3 scale)
+        {
+            return Vector3.Scale(v, scale);
+        }
+
+        /// <summary>
+        /// Returns the distance between this vector and the target vector.
+        /// </summary>
+        public static float Distance(this Vector3 v, Vector3 target)
+        {
+            return Vector3.Distance(v, target);
+        }
+
+        /// <summary>
+        /// Returns the squared distance between this vector and the target vector.
+        /// </summary>
+        public static float SqrDistance(this Vector3 v, Vector3 target)
+        {
+            return (v - target).sqrMagnitude;
+        }
+
+        /// <summary>
+        /// Returns the angle in degrees between this vector and the target vector.
+        /// </summary>
+        public static float Angle(this Vector3 v, Vector3 target)
+        {
+            return Vector3.Angle(v, target);
+        }
+
+        /// <summary>
+        /// Returns the dot product between this vector and the target vector.
+        /// </summary>
+        public static float Dot(this Vector3 v, Vector3 target)
+        {
+            return Vector3.Dot(v, target);
+        }
+
+        /// <summary>
+        /// Returns the cross product between this vector and the target vector.
+        /// </summary>
+        public static Vector3 Cross(this Vector3 v, Vector3 target)
+        {
+            return Vector3.Cross(v, target);
+        }
+
+        /// <summary>
+        /// Returns a copy of this vector with the X component replaced.
+        /// </summary>
+        public static Vector3 WithX(this Vector3 v, float x)
+        {
+            return new Vector3(x, v.y, v.z);
+        }
+
+        /// <summary>
+        /// Returns a copy of this vector with the Y component replaced.
+        /// </summary>
+        public static Vector3 WithY(this Vector3 v, float y)
+        {
+            return new Vector3(v.x, y, v.z);
+        }
+
+        /// <summary>
+        /// Returns a copy of this vector with the Z component replaced.
+        /// </summary>
+        public static Vector3 WithZ(this Vector3 v, float z)
+        {
+            return new Vector3(v.x, v.y, z);
+        }
+
+        /// <summary>
+        /// Returns the projection of this vector onto the target vector.
+        /// </summary>
+        public static Vector3 ProjectOn(this Vector3 v, Vector3 target)
+        {
+            return Vector3.Project(v, target);
+        }
+
+        /// <summary>
+        /// Returns the reflection of this vector off the plane defined by the given normal.
+        /// </summary>
+        public static Vector3 Reflect(this Vector3 v, Vector3 normal)
+        {
+            return Vector3.Reflect(v, normal);
+        }
+
+        /// <summary>
+        /// Converts this Vector3 to a Vector2 by dropping the Z component.
+        /// </summary>
+        public static Vector2 ToVector2(this Vector3 v)
+        {
+            return new Vector2(v.x, v.y);
+        }
+    }
+}
